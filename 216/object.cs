d4b55ce6ee5886f26fdcfceed5d5b750742a2704
UtsavSoni47 <utsavsoni0407@gmail.com>
2025-02-25 10:33:34 +0530

    Add files via upload

---
+++ 216/object.cs
@@ -0,0 +1,16 @@
+using System;
+class xyz
+{
+	public xyz()
+	{
+		Console.WriteLine("constructor");
+	}
+	public static void Main()
+	{
+		xyz obj1=new xyz();
+		xyz obj2=new xyz();
+		Console.ReadLine();
+	}
+}
+
+
