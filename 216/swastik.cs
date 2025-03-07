8ef57b712209da8e61308560b19875ed84bb3df4
UtsavSoni47 <utsavsoni0407@gmail.com>
2025-03-06 23:08:29 -0800

    Update swastik.cs

--- 216/swastik.cs
+++ 216/swastik.cs
@@ -1,75 +1,69 @@
 using System;
 
-class swastik {
-	
-	static void swastika(int row, int col) 
-	{
-		for (int i = 0; i < row; i++) {
-			for (int j = 0; j < col; j++) 
-			{
-				
-				if (i < row / 2) 
-				{
-				
-					if (j < col / 2) 
-					{
-						
-						
-						if (j == 0)
-							Console.Write("*");
-							
-						
-						else
-							Console.Write(" "+ " ");
-					}
-					
-					else if (j == col / 2)
-						Console.Write(" *");
-					
-					else
-					{
-						
-						if (i == 0)
-							Console.Write(" *");
-					}
-				}
-				else if (i == row / 2)
-					Console.Write("* ");
-				else
-				{
-					
-				
-					if (j == col / 2 || j == col - 1)
-						Console.Write("* ");
-					
-					
-					else if (i == row - 1) 
-					{
-						
-						
-						if (j <= col / 2 || j == col - 1)
-							Console.Write("* ");
-						else
-							Console.Write(" "+ " ");
-					} 
-					else
-						Console.Write(" "+" ");
-				}
-			}
-			
-			Console.WriteLine();
-		}
-	} 
-	
-	
-	public static void Main ()
-	{
-		
-	
-		int row = 9, col = 9;
-		
-		
-		swastika(row, col);
-	}
-}
+class swastikPattern
+{
+    static void swastik(int row, int col)
+    {
+        for (int i = 0; i < row; i++)
+        {
+            for (int j = 0; j < col; j++)
+            {
+                if (i < row / 2)
+                {
+                    if (j < col / 2)
+                    {
+                        if (j == 0)
+                            Console.Write("* ");
+                        else
+                            Console.Write("  ");
+                    }
+                    else if (j == col / 2)
+                        Console.Write("* ");
+                    else
+                    {
+                        
+                        if (i == 0)
+                            Console.Write("* ");
+                    }
+                }
+                else if (i == row / 2)
+                    Console.Write("* ");
+                else
+                {
+                  
+                    if (j == col / 2 || j == col - 1)
+                        Console.Write("* ");
+                 
+                    else if (i == row - 1)
+                    {
+                       
+                        if (j <= col / 2 || j == col - 1)
+                            Console.Write("* ");
+                        else
+                            Console.Write("  ");
+                    }
+                    else
+                        Console.Write("  ");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
 
+    public static void Main()
+    {
+        Console.Write("Enter an odd number of rows: ");
+        int row = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter an odd number of columns: ");
+        int col = Convert.ToInt32(Console.ReadLine());
+
+        if (row % 2 == 0 || col % 2 == 0)
+        {
+            Console.WriteLine("Please enter only odd numbers for rows and columns.");
+            return;
+        }
+
+        swastik(row, col);
+    }
+}
